d8de545cac06e40b99ae6d5fcb36651ccb13b873
Jahidul Islam <shukanpukuripec@gmail.com>
2025-04-24 21:45:30 +0600

    All File added

---
+++ lib/ui/screens/login_screen.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:taskmanager/data/models/login_model.dart';
+import 'package:taskmanager/data/service/network_client.dart';
+import 'package:taskmanager/data/utils/urls.dart';
+import 'package:taskmanager/ui/controllers/auth_controller.dart';
+import 'package:taskmanager/ui/screens/forgot_password_verify_email_screen.dart';
+import 'package:taskmanager/ui/screens/main_bottom_nav_screen.dart';
+import 'package:taskmanager/ui/screens/register_screen.dart';
+import 'package:taskmanager/ui/widgets/centered_circular_progress_indicator.dart';
+import 'package:taskmanager/ui/widgets/screen_background.dart';
+import 'package:taskmanager/ui/widgets/snack_bar_message.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _loginInProgress = false;
+
+  // TODO: Validate form(Email & password)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScreenBackground(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 80),
+                Text(
+                  'Get Started With',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 24),
+                TextFormField(
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _emailTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _passwordTEController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Visibility(
+                  visible: _loginInProgress == false,
+                  replacement: const CenteredCircularProgressIndicator(),
+                  child: ElevatedButton(
+                    onPressed: _onTapSignInButton,
+                    child: const Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                Center(
+                  child: Column(
+                    children: [
+                      TextButton(
+                        onPressed: _onTapForgotPasswordButton,
+                        child: const Text('Forgot Password?'),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          style: const TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                          ),
+                          children: [
+                            const TextSpan(text: "Don't have account? "),
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: const TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = _onTapSignUpButton,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTapSignInButton() {
+    if (_formKey.currentState!.validate()) {
+      _login();
+    }
+  }
+
+  Future<void> _login() async {
+    _loginInProgress = true;
+    setState(() {});
+    Map<String, dynamic> requestBody = {
+      "email": _emailTEController.text.trim(),
+      "password": _passwordTEController.text
+    };
+    NetworkResponse response = await NetworkClient.postRequest(
+      url: Urls.loginUrl,
+      body: requestBody,
+    );
+    _loginInProgress = false;
+    setState(() {});
+    if (response.isSuccess) {
+      LoginModel loginModel = LoginModel.fromJson(response.data!);
+      AuthController.saveUserInformation(loginModel.token, loginModel.userModel);
+
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MainBottomNavScreen(),
+        ),
+            (predicate) => false,
+      );
+    } else {
+      showSnackBarMessage(context, response.errorMessage, true);
+    }
+  }
+
+  void _onTapForgotPasswordButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const ForgotPasswordVerifyEmailScreen(),
+      ),
+    );
+  }
+
+  void _onTapSignUpButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const RegisterScreen(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
+  }
+}
